653bf9d74b15703e2f3d48a35d681b882b401680
Fiqihfr21 <fiqihfr@gmail.com>
2020-09-16 17:03:30 +0700

    Finish

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,12 @@ $router->get('/users', 'UserController@index');
 $router->get('/users/{user}', 'UserController@view');
 
 $router->group(['middleware' => 'auth'], function($router) {
-	$router->get('/secure/my-profile', 'SecureController@profile');
+    $router->get('/secure/my-profile', 'SecureController@profile');
 });
+
+$router->post(
+    'auth/login',
+    [
+        'uses' => 'AuthController@authenticate'
+    ]
+);
